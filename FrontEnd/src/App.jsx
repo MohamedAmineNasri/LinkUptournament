e11b6174d6571530819030ca11426140cf306557
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2024-02-20 00:05:49 +0100

    Front

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+import "./App.css";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Blog from "./pages/Blog";
+import Matches from "./pages/Matches";
+import Players from "./pages/Players";
+import Single from "./pages/Single";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/matches" element={<Matches />} />
+        <Route path="/players" element={<Players />} />
+        <Route path="/single" element={<Single />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;

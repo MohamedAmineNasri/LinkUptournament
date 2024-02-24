736f517a5e6fecfc468e95d283f122f307f7e226
yassineghanmi <yassine.ghanmi@esen.tn>
2024-02-24 16:44:15 +0100

    tournements and players crud

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-
+const tournementRouter = require("./Routes/tournementRouter");
+const playerRouter = require("./Routes/playerRouter");
 const app = express();
 
 app.use(cors());
 
+app.use(express.json());
+app.use("/tournement", tournementRouter);
+app.use("/player", playerRouter);
 
 module.exports = app;

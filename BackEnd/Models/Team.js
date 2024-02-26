db2667395b1b1e1334898356f10e272c491a70ef
Mohamed-Mi20 <miaoui.mohamed12@gmail.com>
2024-02-26 11:47:58 +0100

    push

---
+++ BackEnd/Models/Team.js
@@ -0,0 +1,81 @@
+const mongoose = require("mongoose");
+
+const TeamSchema = new mongoose.Schema(
+  {
+    TeamId: Number,
+
+    TeamName: {
+      type: String,
+      require: "AcademyName is required !!",
+    },
+    TeamLogo: {
+      type: String,
+      require: "Logo is required !!",
+    },
+    Total_MatchesWon: {
+      type: Number,
+      default: 0
+    },
+    Total_MatchesLost: {
+      type: Number,
+      default: 0
+    },
+    Total_MatchesDrawn: {
+      type: Number,
+      default: 0
+    },
+    Total_MatchesPlayed: {
+      type: Number,
+      default: 0
+    },
+    Total_Goals_scored: {
+      type: Number,
+      default: 0
+    },
+    Total_Goals_received: {
+      type: Number,
+      default: 0
+    },
+    academy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Academy',
+      // required: true
+    },
+
+    // Group stage data-------------------------------------------------------------------
+    GS_MatchesWon: {
+      type: Number,
+      default: 0
+    },
+    GS_MatchesLost: {
+      type: Number,
+      default: 0
+    },
+    GS_MatchesDrawn: {
+      type: Number,
+      default: 0
+    },
+    GS_MatchesPlayed: {
+      type: Number,
+      default: 0
+    },
+    GS_Goals_scored: {
+      type: Number,
+      default: 0
+    },
+    GS_Goals_received: {
+      type: Number,
+      default: 0
+    },
+    GS_Goals_difference: {
+      type: Number,
+      default: 0
+    },
+    GS_Points: {
+      type: Number,
+      default: 0
+    },
+  },
+);
+
+module.exports = mongoose.model("team", TeamSchema);

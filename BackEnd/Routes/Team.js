7672716d8a7bf9783b883031dc40a869eacb059d
Mohamed-Mi20 <miaoui.mohamed12@gmail.com>
2024-03-01 22:55:39 +0100

    Team services work added

--- BackEnd/Routes/Team.js
+++ BackEnd/Routes/Team.js
@@ -11,10 +11,22 @@ router.get("/getTeam/:id" ,teamService.getTeamById);
 router.delete("/deleteTeam/:id" ,teamService.deleteTeamById);
 
 router.post("/updateMW/:id",teamService.updateTeamMatchesWon);
+router.post("/cancelMW/:id",teamService.cancelTeamMatchesWon);
+
 router.post("/updateML/:id",teamService.updateTeamMatchesLost);
+router.post("/cancelML/:id",teamService.cancelTeamMatchesLost);
+
 router.post("/updateMD/:id",teamService.updateTeamMatchesDrawn);
+router.post("/cancelMD/:id",teamService.cancelTeamMatchesDrawn);
+
 router.post("/updateGoals/:id",teamService.updateGoals_scored);
+router.post("/cancelGoals/:id",teamService.cancelGoals_scored);
+
 router.post("/updateGoalsIn/:id",teamService.updateGoals_received);
+router.post("/cancelGoalsIn/:id",teamService.cancelGoals_received);
+
+router.post("/resetGSdata/:id",teamService.resetGroupStageData);
+
 router.post("/getTeamAndAssaignAcademy",teamService.addTeamAndAssaignToAcademy);
 
 module.exports =router
--- BackEnd/Services/TeamService.js
+++ BackEnd/Services/TeamService.js
@@ -38,32 +38,104 @@ const deleteTeamById =  async (req,res,next)=>{
 
 
 const updateTeamMatchesWon = async (req,res,next)=>{
+    
     const TeamMWData = await Team.findById(req.params.id);
-    TeamMWData.Total_MatchesWon = TeamMWData.Total_MatchesWon + 1;
-    TeamMWData.Total_MatchesPlayed = TeamMWData.Total_MatchesPlayed + 1;
+    //stats
+    TeamMWData.Total_MatchesWon +=  1;
+    TeamMWData.Total_MatchesPlayed +=  1;
+    //Group stage
+    TeamMWData.GS_MatchesWon += 1 
+    TeamMWData.GS_MatchesPlayed +=1 
+    TeamMWData.GS_Points += 3
     await TeamMWData.save()
-    res.json("Team victory increased by 1 sucessfully");
+    res.json(TeamMWData)
+}
+
+const cancelTeamMatchesWon = async (req,res,next)=>{
+
+    const TeamMWData = await Team.findById(req.params.id);
+    if(TeamMWData.Total_MatchesWon == 0 || TeamMWData.Total_MatchesPlayed == 0 || TeamMWData.GS_MatchesPlayed ==0  || TeamMWData.GS_MatchesWon ==0){
+        res.json("you can't decrease further  ")
+    }else{
+        //stats
+        TeamMWData.Total_MatchesWon -= 1;
+        TeamMWData.Total_MatchesPlayed -= 1;
+        //Group stage
+        TeamMWData.GS_MatchesWon -= 1 ;
+        TeamMWData.GS_MatchesPlayed -=1 ;
+        TeamMWData.GS_Points -= 3
+        await TeamMWData.save()
+        res.json(TeamMWData)
+    }
+    
 }
 
 
 
 const updateTeamMatchesLost = async (req,res,next)=>{
+   
     const TeamMLData = await Team.findById(req.params.id);
-    TeamMLData.Total_MatchesLost = TeamMLData.Total_MatchesLost + 1;
-    TeamMLData.Total_MatchesPlayed = TeamMLData.Total_MatchesPlayed + 1;
+    //stats
+    TeamMLData.Total_MatchesLost +=  1;
+    TeamMLData.Total_MatchesPlayed += 1;
+    //Group stage
+    TeamMLData.GS_MatchesLost +=  1;
+    TeamMLData.GS_MatchesPlayed += 1;
+    TeamMLData.GS_Points += 0
+
     await TeamMLData.save()
-    res.json("Team Lost increased by 1 sucessfully");
+    res.json(TeamMLData)
+}
+const cancelTeamMatchesLost = async (req,res,next)=>{
+
+    const TeamMLData = await Team.findById(req.params.id);
+    if(TeamMLData.Total_MatchesLost == 0 || TeamMLData.Total_MatchesPlayed == 0 || TeamMLData.GS_MatchesPlayed ==0 || TeamMLData.GS_MatchesLost ==0){
+        res.json("you can't decrease further  ")
+    }else{
+
+        //stats
+        TeamMLData.Total_MatchesLost -= 1;
+        TeamMLData.Total_MatchesPlayed -= 1;
+        
+        //Group stage
+        TeamMLData.GS_MatchesLost -=  1;
+        TeamMLData.GS_MatchesPlayed -= 1;
+        
+        await TeamMLData.save()
+        res.json(TeamMLData)
+    }
 }
 
 
 
-
 const updateTeamMatchesDrawn = async (req,res,next)=>{
+
     const TeamMDData = await Team.findById(req.params.id);
-    TeamMDData.Total_MatchesDrawn = TeamMDData.Total_MatchesDrawn + 1;
-    TeamMDData.Total_MatchesPlayed = TeamMDData.Total_MatchesPlayed + 1;
+    //stats
+    TeamMDData.Total_MatchesDrawn += 1;
+    TeamMDData.Total_MatchesPlayed += 1;
+    //Group stage
+    TeamMDData.GS_MatchesDrawn+= 1;
+    TeamMDData.GS_MatchesPlayed += 1;
+    TeamMDData.GS_Points += 1
     await TeamMDData.save()
-    res.json("Team draw increased by 1 sucessfully");
+    res.json(TeamMDData)
+}
+const cancelTeamMatchesDrawn = async (req,res,next)=>{
+    const TeamMDData = await Team.findById(req.params.id);
+    if(TeamMDData.Total_MatchesDrawn  == 0 || TeamMDData.Total_MatchesPlayed == 0 || TeamMDData.GS_MatchesPlayed ==0||TeamMDData.GS_MatchesDrawn ==0 ){
+        res.json("you can't decrease further  ")
+    }else{
+        //stats
+        TeamMDData.Total_MatchesDrawn -= 1;
+        TeamMDData.Total_MatchesPlayed -= 1;
+        //Group stage
+        TeamMDData.GS_MatchesDrawn -= 1;
+        TeamMDData.GS_MatchesPlayed -= 1;
+        TeamMDData.GS_Points -= 1
+        await TeamMDData.save()
+        res.json(TeamMDData)
+    }
 }
 
 
@@ -71,19 +143,80 @@ const updateTeamMatchesDrawn = async (req,res,next)=>{
 const updateGoals_scored = async (req,res, next) => {
     
         const TeamData = await Team.findById(req.params.id);
-        TeamData.Total_Goals_scored += 2; // test
+        //stats
+        TeamData.Total_Goals_scored += 1; 
+        //Group Stage
+        TeamData.GS_Goals_scored += 1; 
+        TeamData.GS_Goals_difference = TeamData.GS_Goals_scored - TeamData.GS_Goals_received; 
         await TeamData.save();
-        res.json("Goals numbers increased sucessfully");
+        res.json(TeamData)
    
 };
+const cancelGoals_scored = async (req,res, next) => {
+    
+        const TeamData = await Team.findById(req.params.id);
+        if(TeamData.Total_Goals_scored ==0 || TeamData.GS_Goals_scored ==0){
+            res.json("you can't decrease further")
+        }else{
+        //stats
+        TeamData.Total_Goals_scored -= 1;
+        //Group Stage
+        TeamData.GS_Goals_scored -= 1; 
+        TeamData.GS_Goals_difference = TeamData.GS_Goals_scored - TeamData.GS_Goals_received;
+        await TeamData.save();
+        res.json(TeamData)
+        }
+   
+};
+
+
+
 
 const updateGoals_received = async (req,res, next) => {
     
         const TeamData = await Team.findById(req.params.id);
-        TeamData.Total_Goals_received += 2; 
+        //stats
+        TeamData.Total_Goals_received += 1; 
+        //Group stage
+        TeamData.GS_Goals_received += 1; 
+        TeamData.GS_Goals_difference = TeamData.GS_Goals_scored - TeamData.GS_Goals_received;
         await TeamData.save();
-        res.json("Goals received  numbers increased sucessfully");
+        res.json(TeamData)
    
+};
+const cancelGoals_received = async (req,res, next) => {
+    
+        const TeamData = await Team.findById(req.params.id);
+        if(TeamData.Total_Goals_received == 0 || TeamData.GS_Goals_received ==0){
+            res.json("you can't decrease further")
+        }else{
+            //stats
+            TeamData.Total_Goals_received -= 1; 
+            //Group stage
+            TeamData.GS_Goals_received -= 1;  
+            TeamData.GS_Goals_difference = TeamData.GS_Goals_scored - TeamData.GS_Goals_received;
+        await TeamData.save();
+        res.json(TeamData)
+        }
+};
+
+
+
+
+const resetGroupStageData = async (req,res, next) => {
+    
+        const TeamData = await Team.findById(req.params.id);
+        TeamData.GS_MatchesWon = 0;
+        TeamData.GS_MatchesLost = 0;
+        TeamData.GS_MatchesDrawn = 0;
+        TeamData.GS_MatchesPlayed = 0;
+        TeamData.GS_Goals_scored = 0;
+        TeamData.GS_Goals_difference = 0;
+        TeamData.GS_Goals_received = 0;
+        TeamData.GS_Points = 0;  
+        await TeamData.save();
+        res.json(TeamData)
+        
 };
 
 
@@ -91,4 +224,4 @@ const updateGoals_received = async (req,res, next) => {
 
 
 
-module.exports = { getAllTeams,addTeam, deleteTeamById, getTeamById,updateTeamMatchesWon,updateTeamMatchesLost,updateTeamMatchesDrawn,updateGoals_scored,updateGoals_received,addTeamAndAssaignToAcademy };
+module.exports = { getAllTeams,addTeam, deleteTeamById, getTeamById,updateTeamMatchesWon,updateTeamMatchesLost,updateTeamMatchesDrawn,updateGoals_scored,updateGoals_received,addTeamAndAssaignToAcademy,cancelTeamMatchesWon,cancelTeamMatchesLost,cancelTeamMatchesDrawn,cancelGoals_received,cancelGoals_scored,resetGroupStageData };

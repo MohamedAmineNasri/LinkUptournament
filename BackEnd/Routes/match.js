8ceaf81522ee0c1bd969391f15664389305320cf
hamhoum10 <omriyasser12@gmail.com>
2024-03-03 18:41:25 +0100

    update match comleate

--- BackEnd/Routes/match.js
+++ BackEnd/Routes/match.js
@@ -10,9 +10,9 @@ router.get("/", matchController.getAllematch);
 router.get("/:id", matchController.getmatchById);
 router.put("/:id", matchController.updatematchById);
 router.delete("/:id", matchController.deletematchById);
-router.put("/score1/:id", matchController.updatescoreById);
-router.put("/score2/:id", matchController.updatescore2ById);
-router.put("/score1min/:id", matchController.updatescore2_ById);
-router.put("/score2min/:id", matchController.updatescore_ById);
+router.put(":id/score1/", matchController.updatescoreById);
+router.put("/:id/score2", matchController.updatescore2ById);
+router.put("/:id/score1min", matchController.updatescore2_ById);
+router.put("/:id/score2min", matchController.updatescore_ById);
 
 module.exports = router;

a9d89594bb65b33a38b9d0cbd996c1dd90dc26f9
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2024-03-09 18:54:10 +0100

    feat: Added user deletion with confirmation dialog

--- FrontEnd/Features/users/usersApiSlice.js
+++ FrontEnd/Features/users/usersApiSlice.js
@@ -1,14 +1,21 @@
 import { apiSlice } from "../../app/api/apiSlice";
 
 export const usersApiSlice = apiSlice.injectEndpoints({
-    endpoints: builder => ({
+        endpoints: (builder) => ({
         getUsers: builder.query({
-            query: () => '/user/users',
+            query: () => "/user/users",
             keepUnusedDataFor: 5,
-        })
-    })
-})
+        }),
+        deleteUserById: builder.mutation({
+            query: (userId) => ({
+            url: `/user/users/${userId}`,
+            method: "DELETE",
+            }),
+        }),
+        }),
+    });
 
 export const {
-    useGetUsersQuery
+    useGetUsersQuery,
+    useDeleteUserByIdMutation 
 } = usersApiSlice 
--- FrontEnd/src/pages/User Tables/UsersTable.tsx
+++ FrontEnd/src/pages/User Tables/UsersTable.tsx
@@ -4,21 +4,57 @@
     import BrandThree from '../../Dashboard/src/images/brand/brand-03.svg';
     import BrandFour from '../../Dashboard/src/images/brand/brand-04.svg';
     import BrandFive from '../../Dashboard/src/images/brand/brand-05.svg';
-    import { useGetUsersQuery } from "../../../Features/users/usersApiSlice.js";
+    import { useGetUsersQuery,useDeleteUserByIdMutation  } from "../../../Features/users/usersApiSlice.js";
     import ReactWhatsapp from 'react-whatsapp';
     import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
     import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-    
+    import { faUserMinus } from '@fortawesome/free-solid-svg-icons';
+    import { faUserPen } from '@fortawesome/free-solid-svg-icons';
+    import { useState } from 'react';
+    import {
+        Button,
+        Dialog,
+        DialogActions,
+        DialogContent,
+        DialogContentText,
+        DialogTitle,
+    } from '@mui/material';
 
-    const UsersTable = () => {
-        const {
+
+        const UsersTable = () => {
+            const {
             data: users,
             isLoading,
             isSuccess,
             isError,
             error,
-        } = useGetUsersQuery();
-    return (
+            } = useGetUsersQuery();
+            const [deleteUserId, setDeleteUserId] = useState(null);
+            const [deleteUserById] = useDeleteUserByIdMutation();
+            const [openConfirmationDialog, setOpenConfirmationDialog] = useState(false);
+        
+            const handleDeleteUser = async () => {
+            try {
+                await deleteUserById(deleteUserId);
+                console.log("User deleted: " + deleteUserId);
+                setDeleteUserId(null);
+                setOpenConfirmationDialog(false);
+            } catch (error) {
+                console.error("Error deleting user:", error);
+            }
+            };
+        
+            const handleClickOpenConfirmationDialog = (userId) => {
+            setDeleteUserId(userId);
+            setOpenConfirmationDialog(true);
+            };
+        
+            const handleCloseConfirmationDialog = () => {
+            setDeleteUserId(null);
+            setOpenConfirmationDialog(false);
+            };
+
+        return (
         <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
         <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
             Users Accounts
@@ -65,8 +101,12 @@
             >
                 <div className="flex items-center gap-3 p-2.5 xl:p-5">
                     <p className="hidden text-black dark:text-white sm:block">
-                        {user.firstName} {user.lastName}
+                        {user.firstName} {user.lastName} 
                     </p>
+                    <FontAwesomeIcon icon={faUserPen} />
+                    <button onClick={() => handleClickOpenConfirmationDialog(user._id)}>
+                    <FontAwesomeIcon icon={faUserMinus} />
+                    </button>
                 </div>
                 <div className="flex items-center justify-center p-2.5 sm:flex xl:p-5">
                 {new Date(user.birthday).toLocaleDateString()}
@@ -88,6 +128,23 @@
             </div>
             ))}
         </div>
+        <Dialog
+        open={openConfirmationDialog}
+        onClose={handleCloseConfirmationDialog}
+        >
+            <DialogTitle>Confirmation</DialogTitle>
+            <DialogContent>
+            <DialogContentText>
+                Are you sure you want to delete this user?
+            </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+            <Button onClick={handleCloseConfirmationDialog}>Cancel</Button>
+            <Button onClick={handleDeleteUser} autoFocus>
+                Delete
+            </Button>
+            </DialogActions>
+        </Dialog>
         </div>
     );
     };

68b7f8335b6d289caacfd76eb7da99589e44733e
hamhoum10 <omriyasser12@gmail.com>
2024-03-12 17:04:41 +0100

    .

--- BackEnd/Models/match.js
+++ BackEnd/Models/match.js
@@ -6,20 +6,9 @@ var match = new Schema({
     matchType: String,
     weatherCondition: String,
     score: [Number],    
-    injuries:[{type: mongoose.Schema.Types.ObjectId,
-        ref: 'Player',}],
     card:{playername:String,playernumber:Number,red:Number,yellow:Number},
     extraTime:Number,
     matchStatus:String,
-    team1: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Teams',
-        // required: true
-      },
-      team2: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Teams',
-        // required: true
-      }
+   
 });
 module.exports = mongoose.model('match', match)

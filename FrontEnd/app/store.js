abc95fdd41d3f596d234bff3e2d99ccead163f96
Mohamed-Mi20 <miaoui.mohamed12@gmail.com>
2024-03-14 16:16:05 +0100

    no conflicts

--- FrontEnd/app/store.js
+++ FrontEnd/app/store.js
@@ -1,16 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit"; 
-import { apiSlice } from "./api/apiSlice"; 
-import authReducer from '../Features/auth/authSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./api/apiSlice";
+import authReducer from '../Features/auth/authSlice';
+import rootReducer from '../src/redux/rootReducer'; // Import the rootReducer
 
-
-export const store =  configureStore({
+export const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
-        auth: authReducer
+        auth: authReducer,
+        root: rootReducer // Include the rootReducer here
     },
     middleware: getDefaultMiddleware =>
-  getDefaultMiddleware().concat(apiSlice.middleware),
-
-    devTools: true 
+        getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true
     // in production it will be turned into false
-})
+});
+
+export default store;
--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-// import store from "../src/redux/store.js";
+import storeN2 from "../src/redux/store.js";
 import { store } from "../app/store.js";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";

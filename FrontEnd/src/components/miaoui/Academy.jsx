9a688b66114f0e311806366dc9ea7ab2dec0a01c
Mohamed-Mi20 <miaoui.mohamed12@gmail.com>
2024-03-27 17:51:40 +0100

    navbar compoenet (not final version)

--- FrontEnd/src/components/miaoui/Academy.jsx
+++ FrontEnd/src/components/miaoui/Academy.jsx
@@ -6,6 +6,7 @@ import { fetchAcademybyManagerId } from "../../redux/slice/academySlice";
 import academyImagespectators from "../../assets/Mi-imgs/1.jpg";
 import academyImageteam from "../../assets/Mi-imgs/team1.jpg";
 import { Link } from "react-router-dom/dist/umd/react-router-dom.development";
+import HeaderNavBar from "./HeaderNavBar";
 
 export const Academy = () => {
   const dispatch = useDispatch();
@@ -57,71 +58,8 @@ export const Academy = () => {
   return (
     <div>
       <div className="site-wrap">
+        <HeaderNavBar></HeaderNavBar>
         {/* header/overlay image */}
-        <div>
-          <header className="site-navbar py-4" role="banner">
-            <div className="container-fluid">
-              <div className="d-flex align-items-center">
-                <div className="site-logo">
-                  <a href="index.html">
-                    <img src="/public/assets/images/logo.png" alt="Logo" />
-                  </a>
-                </div>
-                <div className="ml-auto">
-                  <nav
-                    className="site-navigation position-relative text-right"
-                    role="navigation"
-                  >
-                    <ul className="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
-                      <li>
-                        <Link to="/" className="nav-link">
-                          Home
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to="/tests" className="nav-link">
-                          Match Cards
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to="/a" className="nav-link">
-                          Match Time
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to="/addAcademy" className="nav-link">
-                          Academy Creation
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to="/Academy" className="nav-link">
-                          Academies
-                        </Link>
-                      </li>
-                      <li className="active">
-                        <Link to="/signin" className="nav-link">
-                          Signup
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to="/profile" className="nav-link">
-                          Profile
-                        </Link>
-                      </li>
-                    </ul>
-                  </nav>
-
-                  <a
-                    href="#"
-                    className="d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black float-right text-white"
-                  >
-                    <span className="icon-menu h3 text-white"></span>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </header>
-        </div>
         <div
           className="hero overlay"
           style={{ backgroundImage: `url(${academyImagespectators})` }}
--- FrontEnd/src/components/miaoui/AddAcademy.jsx
+++ FrontEnd/src/components/miaoui/AddAcademy.jsx
@@ -6,7 +6,7 @@ import { convertToBase64 } from "../../utilities/convertFileBase64";
 import addformstadiumImage from "../../assets/Mi-imgs/2.jpg";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import { Link } from "react-router-dom/dist/umd/react-router-dom.development";
+import HeaderNavBar from "./HeaderNavBar";
 
 export const AddAcademy = () => {
   const dispatch = useDispatch();
@@ -205,71 +205,7 @@ export const AddAcademy = () => {
   };
   return (
     <div>
-      <div>
-        <header className="site-navbar py-4" role="banner">
-          <div className="container-fluid">
-            <div className="d-flex align-items-center">
-              <div className="site-logo">
-                <a href="index.html">
-                  <img src="/public/assets/images/logo.png" alt="Logo" />
-                </a>
-              </div>
-              <div className="ml-auto">
-                <nav
-                  className="site-navigation position-relative text-right"
-                  role="navigation"
-                >
-                  <ul className="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
-                    <li>
-                      <Link to="/" className="nav-link">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/tests" className="nav-link">
-                        Match Cards
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/a" className="nav-link">
-                        Match Time
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/addAcademy" className="nav-link">
-                        Academy Creation
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/Academy" className="nav-link">
-                        Academies
-                      </Link>
-                    </li>
-                    <li className="active">
-                      <Link to="/signin" className="nav-link">
-                        Signup
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/profile" className="nav-link">
-                        Profile
-                      </Link>
-                    </li>
-                  </ul>
-                </nav>
-
-                <a
-                  href="#"
-                  className="d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black float-right text-white"
-                >
-                  <span className="icon-menu h3 text-white"></span>
-                </a>
-              </div>
-            </div>
-          </div>
-        </header>
-      </div>
-
+      <HeaderNavBar></HeaderNavBar>
       {/* Hero image ------------------------- */}
       <div
         className="hero overlay2 HeroImageAddAcademy"

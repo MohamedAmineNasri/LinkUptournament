59da19a017a9160ce341e2757f67ca407f89aee6
hamhoum <54715039+hamhoum10@users.noreply.github.com>
2024-04-21 17:25:32 +0100

    Update vite.config.js

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,14 +1,9 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "jsm-l9",
-    project: "javascript-react"
-  })],
-
-  build: {
-    sourcemap: true
-  }
+  server: {
+    host: '127.0.0.1',
+  },
+  plugins: [react()],
 });

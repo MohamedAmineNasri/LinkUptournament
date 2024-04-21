2e4c93077100651c50fca87c230353f31cc748cc
hamhoum <54715039+hamhoum10@users.noreply.github.com>
2024-04-21 17:22:51 +0100

    Update allowedOrigins.js

--- BackEnd/config/allowedOrigins.js
+++ BackEnd/config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
     'http://127.0.0.1:5174',
     'http://localhost:8000',
     'http://localhost:3000',
-    'http://localhost:5173',
+    'http://localhost:5173'
 ];
 
 module.exports = allowedOrigins;

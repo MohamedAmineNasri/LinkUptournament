1be3c9322b882e6fecf79959c1d24a8b3d4fafa8
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2024-05-18 22:07:48 +0100

    Updating Cros

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -84,7 +84,7 @@ app.use(cors(corsOptions));
 
 app.use(express.static("public"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.use(cors(corsOptions));
 
